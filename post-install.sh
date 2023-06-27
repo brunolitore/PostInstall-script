1849dbe566d3d68c6823a5f7a3058a9acb8a8a20
Bruno Dias <brunophm@gmail.com>
2023-06-27 15:17:03 -0300

    :sparkles: Feat: added installation of flatpak apps

--- post-install.sh
+++ post-install.sh
@@ -17,7 +17,57 @@ if [ "$packageManager" = "apt" ]; then
         echo "Flatpak installed successfully."
     else
         echo "Flatpak is already installed."
+        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
     fi
+
+    flatpak install flathub com.belmoussaoui.Authenticator \
+      com.bitwarden.desktop \
+      com.borgbase.Vorta \
+      com.brave.Browser \
+      com.discordapp.Discord \
+      com.github.tchx84.Flatseal \
+      com.mattjakeman.ExtensionManager \
+      com.microsoft.Edge \
+      com.nextcloud.desktopclient.nextcloud \
+      com.notesnook.Notesnook \
+      com.slack.Slack \
+      com.spotify.Client \
+      com.usebottles.bottles \
+      com.valvesoftware.Steam \
+      hu.kramo.Cartridges \
+      io.appflowy.AppFlowy \
+      io.bassi.Amberol \
+      io.github.Foldex.AdwSteamGtk \
+      io.github.celluloid_player.Celluloid \
+      io.github.mimbrero.WhatsAppDesktop \
+      io.github.seadve.Kooha \
+      io.gitlab.adhami3310.Impression \
+      io.gitlab.zehkira.Monophony \
+      it.mijorus.gearlever \
+      md.obsidian.Obsidian \
+      net.lutris.Lutris \
+      org.gnome.Boxes \
+      org.gnome.Evince \
+      org.gnome.FileRoller \
+      org.gnome.Loupe \
+      org.gnome.SoundRecorder \
+      org.gnome.TextEditor \
+      org.gnome.baobab \
+      org.gnome.clocks \
+      io.github.amit9838.weather \
+      org.inkscape.Inkscape \
+      org.kde.kdenlive \
+      org.libretro.RetroArch \
+      org.mozilla.Thunderbird \
+      org.nickvision.money \
+      org.olivevideoeditor.Olive \
+      org.onlyoffice.desktopeditors \
+      org.qbittorrent.qBittorrent \
+      org.telegram.desktop \
+      re.sonny.Tangram \
+      us.zoom.Zoom -y
+
 elif [ "$packageManager" = "dnf" ]; then
     if ! command -v flatpak &> /dev/null; then
         echo "Flatpak is not installed. Installing Flatpak..."
@@ -31,7 +81,57 @@ elif [ "$packageManager" = "dnf" ]; then
         echo "Flatpak installed successfully."
     else
         echo "Flatpak is already installed."
+        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
     fi
+    
+    flatpak install flathub com.belmoussaoui.Authenticator \
+      com.bitwarden.desktop \
+      com.borgbase.Vorta \
+      com.brave.Browser \
+      com.discordapp.Discord \
+      com.github.tchx84.Flatseal \
+      com.mattjakeman.ExtensionManager \
+      com.microsoft.Edge \
+      com.nextcloud.desktopclient.nextcloud \
+      com.notesnook.Notesnook \
+      com.slack.Slack \
+      com.spotify.Client \
+      com.usebottles.bottles \
+      com.valvesoftware.Steam \
+      hu.kramo.Cartridges \
+      io.appflowy.AppFlowy \
+      io.bassi.Amberol \
+      io.github.Foldex.AdwSteamGtk \
+      io.github.celluloid_player.Celluloid \
+      io.github.mimbrero.WhatsAppDesktop \
+      io.github.seadve.Kooha \
+      io.gitlab.adhami3310.Impression \
+      io.gitlab.zehkira.Monophony \
+      it.mijorus.gearlever \
+      md.obsidian.Obsidian \
+      net.lutris.Lutris \
+      org.gnome.Boxes \
+      org.gnome.Evince \
+      org.gnome.FileRoller \
+      org.gnome.Loupe \
+      org.gnome.SoundRecorder \
+      org.gnome.TextEditor \
+      org.gnome.baobab \
+      org.gnome.clocks \
+      io.github.amit9838.weather \
+      org.inkscape.Inkscape \
+      org.kde.kdenlive \
+      org.libretro.RetroArch \
+      org.mozilla.Thunderbird \
+      org.nickvision.money \
+      org.olivevideoeditor.Olive \
+      org.onlyoffice.desktopeditors \
+      org.qbittorrent.qBittorrent \
+      org.telegram.desktop \
+      re.sonny.Tangram \
+      us.zoom.Zoom -y
+
 else
     echo "Invalid package manager specified."
 fi
